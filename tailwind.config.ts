bc1ff69a4dabece4e0d64b02c1f574b09baad050
sfox123 <shagyfox133@gmail.com>
2025-10-15 17:40:09 +0530

    tsChange4

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,7 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     container: { center: true, padding: "1rem" },
