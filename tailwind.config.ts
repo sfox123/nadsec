38e32155240db0437e906b846689f445cc109387
sfox123 <shagyfox133@gmail.com>
2025-10-15 17:41:32 +0530

    tsChange5

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,9 +3,8 @@ import type { Config } from "tailwindcss";
 module.exports = {
   content: [
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/Components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     container: { center: true, padding: "1rem" },
